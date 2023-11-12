7a67180173822208a4fb78f6777c04f30d5e5221
yarikkot04 <yarikkotenkoim13@gmail.com>
2023-11-12 22:09:24 +0200

    Added express-handlebars.
    Added middleware for the correct operation of the application.
    Added main page route.

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express')
+const expHbs = require('express-handlebars')
+const path = require('path')
+
+const mainRoute = require('./routes/main')
+
+const hbs = expHbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
 const app = express()
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'pages')
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', mainRoute)
+
 
 function start() {
     const PORT = process.env.PORT || 3000
